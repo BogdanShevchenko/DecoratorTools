37d787e1b35465f087a9b75333b5cd277fa9c1a4
BogdanShevchenko <bogdan.shevchenko42@gmail.com>
2023-03-03 19:16:56 +0200

    some comments

--- parse.py
+++ parse.py
@@ -12,17 +12,20 @@ def get_args_dict(f: Callable, args: Sequence, kwargs: dict) -> dict:
     parameter in resulting dict with all unpacked arguments,
     and also all this arguments will be in resulting dict by themselves
     """
+    # co_varnames is list of all variables in function in order of definition, only order exception is args
     var_names = f.__code__.co_varnames
+    # co_argcount if number of all named parameters before *args, or before **kwargs (or all parameters if no **kwargs)
     pos_arg_count = min(f.__code__.co_argcount, len(args))
+    # 3 and 4 bit of flags indicates if there are *args and **kwargs unpacking in function
     flags = '{:04b}'.format(f.__code__.co_flags)
     has_args = int(flags[-3])
     has_kwargs = int(flags[-4])
     kwarg_name = [None, var_names[-1]][has_kwargs]
+    # *args is always after all other named variables but before kwargs
     arg_name = [None, var_names[-1 - has_kwargs]][has_args]
     kwargs.update(zip(var_names[:pos_arg_count], args[:pos_arg_count]))
     if has_kwargs:
         kwargs[kwarg_name] = {i: kwargs[i] for i in kwargs if (i not in var_names) or (i in [arg_name, kwarg_name])}
-
     if has_args:
         kwargs[arg_name] = args[pos_arg_count:]
         defaults = f.__kwdefaults__ or {}
